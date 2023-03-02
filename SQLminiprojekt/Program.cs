6ab173f7166a1c7e95bfa615f4d5ae4f84287138
Wettergrund <jonas.wettergrund@gmail.com>
2023-03-02 14:40:26 +0100

    Add project files.

---
+++ SQLminiprojekt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SQLminiprojekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
