d9227e466a483a53d60d27350d11c1c7a8818e30
Wettergrund <jonas.wettergrund@gmail.com>
2023-03-02 19:00:57 +0100

    Started to add basic functionality: - Basic menu system - Add user function - Founation for a remove user function

--- SQLminiprojekt/Program.cs
+++ SQLminiprojekt/Program.cs
@@ -1,5 +1,5 @@
 ﻿global using SQLminiprojekt.Models;
-using SQLminiprojekt.Data;
+global using SQLminiprojekt.Data;
 
 namespace SQLminiprojekt
 {
@@ -7,34 +7,180 @@ namespace SQLminiprojekt
     {
         static void Main(string[] args)
         {
+            Run();
+
             // Test connection
 
-            List<UserModel> users = DBconnection.GetAllUsers();
+            //List<UserModel> users = DBconnection.GetAllUsers();
 
-            for (int i = 0; i < users.Count; i++)
-            {
-                Console.WriteLine($"User {i + 1} : {users[i].Person_Name} ");
-            }
+            //string[] allUsers = ConertToArray(users);
+
+            //int selectedOption = Menu(allUsers);
+
+            //Console.WriteLine(selectedOption);
+            //Console.ReadLine();
 
         }
 
         static void Run()
         {
-            /* Menu shoult contain
+            /* Menu should contain
              * 
              * Registrera arbetstid -> TimeReporting()
-             * Modify Users -> users.add(), users.remove()
+             * Modify Users -> user.add(), user.remove()
              * Modify projects -> projects.add(), project.remove()
              * 
              * 
              */
+
+            string[] mainMenu = {
+                "Registrera arbetstid",
+                "Ändra användare",
+                "Ändra projekt"
+            };
+
+            int selectedItem = Menu(mainMenu);
+
+            bool runProgram = true;
+
+            while (runProgram)
+            {
+                switch (selectedItem)
+                {
+                    case -1:
+                        runProgram = false;
+                        break;
+                    case 0:
+                        TimeReporting();
+                        break;
+                    case 1:
+                        ModifyUser();
+                        break;
+                    case 2:
+                        ModifyProject();
+                        break;
+                            
+                }
+            }
+
         }
 
-        static int Menu(List<string> menuItems)
+        static void TimeReporting()
         {
-        
+            List<UserModel> users = DBconnection.GetAllUsers();
+            string[] allUsers = ConertToArray(users);
+            List<ProjectModel> projects = DBconnection.GetAllProjects();
+            string[] allProjects = ConertToArray(projects);
 
-            return -1;
+            Console.WriteLine("Välj vilken användare du ska rapportera på");
+            int selectedUser = Menu(allUsers);
+
+            Console.WriteLine("Ange vilket projekt");
+            int selectedProject = Menu(allProjects);
+
+            Console.WriteLine("Ange hela timmar:");
+            int hours = int.Parse(Console.ReadLine());
+        
+            
+
+        }
+        static void ModifyUser()
+        {
+            string[] UserMenu = { 
+                "Lägg till användare", // --> user.add()
+                "Ta bort användare " // --> user.remove()
+            };
+
+            int selectedItem = Menu(UserMenu);
+
+            bool runProgram = true;
+
+            while (runProgram)
+            {
+                switch (selectedItem)
+                {
+                    case -1:
+                        runProgram = false;
+                        break;
+                    case 0:
+                        User.Add();
+
+                        break;
+                    case 1:
+                        User.Remove();
+                        break;
+                   
+
+                }
+            }
+
+
+        }
+        static void ModifyProject()
+        {
+
+            string[] UserMenu = {
+                "Lägg till projekt", // --> user.add()
+                "Ta bort projekt " // --> user.remove()
+            };
+
+        }
+
+
+        static string[] ConertToArray(List<UserModel> input)
+        {
+
+            int numberOfItems = input.Count;
+
+            string[] strings = new string[numberOfItems];
+
+            for (int i = 0; i < numberOfItems; i++)
+            {
+                strings[i] = input[i].Person_Name;
+            }
+
+            return strings;
+        }
+
+        static string[] ConertToArray(List<ProjectModel> input)
+        {
+
+            int numberOfItems = input.Count;
+
+            string[] strings = new string[numberOfItems];
+
+            for (int i = 0; i < numberOfItems; i++)
+            {
+                strings[i] = input[i].Project_Name;
+            }
+
+            return strings;
+        }
+
+
+        static int Menu(string[] input)
+        {
+            Console.Clear();
+
+            int stringLength = input.Length;
+
+            for (int i = 0; i < stringLength; i++)
+            {
+                Console.WriteLine($"[{i + 1}] {input[i]}");
+            }
+            Console.WriteLine($"[{stringLength + 1}] Gå tillbaka");
+
+            Console.Write("Välj en siffra: ");
+            int selectedItem = int.Parse(Console.ReadLine());
+            if(selectedItem == stringLength + 1) {
+
+                return -1;
+
+            }
+
+            selectedItem--;
+          
+            return selectedItem;
         }
     }
 }
